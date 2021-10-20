4bf787c0e8ad70d424b0f45a7f47b450e0696527
Olzhas Romazanov <96romrom@gmail.com>
2021-10-20 20:39:56 +0600

    added html to template

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,219 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="page">
+        <div class="page__main main">
+            <!-- HEADER -->
+            <header class="main__header header">
+                <div class="logo">
+                    <img class="logo__img" src="./images/mainpage/logo.svg"
+                    width="215px" height="35px" alt="Kolesa Group Logo">
+                </div>
+                <div class="header__activebar">
+                    <div class="searchbar">
+                        <button class="searchbar__button">
+                            <img src="./images/mainpage/search.svg"
+                            width="18px" height="18px" alt="search image">
+                        </button>
+                        <input class="searchbar__input" type="text" placeholder="Поиск">
+                    </div>
+                    <a href="#" class="userbar">
+                        <img class="userbar__img" src="./images/mainpage/user.svg"
+                        width="44px" height="44px" alt="user image">
+                        <span class="userbar__info">
+                            <p class="userbar__name">Мортиджан</p>
+                            <p class="userbar__points">300 баллов</p>
+                        </span>
+                    </a>
+                </div>
+            </header>
+            <div class="main__showcase">
+                <!-- NAVBAR -->
+                <nav class="main__nav nav">
+                    <ul class="nav__list">
+                        <li class="nav__item"><a class="nav__link" href="#">Оргсхема</a></li>
+                        <li class="nav__item">
+                          <a class="nav__link" href="#">Kolesa
+                            <span class="nav_link nav__link--bold">Team</span>
+                          </a>
+                        </li>
+                        <li class="nav__item nav__item--active">
+                          <a class="nav__link nav__link--bold" href="#">Kolesa Shop</a>
+                        </li>
+                        <li class="nav__item">
+                          <a class="nav__link" href="#">Картина компании</a>
+                        </li>
+                        <li class="nav__item"><a class="nav__link" href="#">Новости</a></li>
+                        <li class="nav__item"><a class="nav__link" href="#">Education</a></li>
+                        <li class="nav__item"><a class="nav__link" href="#">Guidelines</a></li>
+                        <li class="nav__item"><a class="nav__link" href="#">Библиотека</a></li>
+                        <li class="nav__item"><a class="nav__link" href="#">FAQ</a></li>
+                    </ul>
+                </nav>
+                <!-- SECTION -->
+                <section class="section">
+                    <div class="section__banner banner">
+                        <img class="banner_img" src="./images/mainpage/banner.svg"
+                        width="1040px" height="335px" alt="banner">
+                    </div>
+                    <div class="section__hotbuttons">
+                        <button class="hotbutton hotbutton--green">
+                            <img class="hotbutton__img" src="./images/mainpage/plus.svg"
+                            width="48px" height="56px" alt="plus sign">
+                            <span class="hotbutton__text">Получить баллы</span>
+                        </button>
+                        <button class="hotbutton hotbutton--yellow">
+                            <img class="hotbutton__img" src="./images/mainpage/question.svg"
+                            width="48px" height="56px" alt="question mark">
+                            <span class="hotbutton__text">Как получить баллы</span>
+                        </button>
+                        <button class="hotbutton hotbutton--blue">
+                            <img class="hotbutton__img" src="./images/mainpage/gift.svg"
+                            width="48px" height="56px" alt="gift">
+                            <span class="hotbutton__text">Подарить баллы</span>
+                        </button>
+                    </div>
+                    <div class="section__radiobuttons radiobuttons">
+                        <input class="radiobuttons__input js__input--all"
+                        type="radio" name="category" id="all-items">
+                        <label class="radiobuttons__label" for="all-items">Все товары</label>
+                        <input class="radiobuttons__input js__input--clothes"
+                        type="radio" name="category" id="clothes">
+                        <label class="radiobuttons__label" for="clothes">Одежда</label>
+                        <input class="radiobuttons__input js__input--accessories"
+                        type="radio" name="category" id="accessories">
+                        <label class="radiobuttons__label" for="accessories">Аксессуары</label>
+                    </div>
+                    <div class="section__catalog catalog js__catalog">
+                        <!-- <div class="catalog__card card">
+                            <img class="card__img" src="./images/mainpage/tshirt.svg"
+                            width="330px" height="330px" alt="Kolesa t-shirt">
+                            <p class="card__points">220 баллов</p>
+                            <p class="card__description">Футболка "Эволюционируй или ...</p>
+                            <p class="card__sizes">Размеры S/M/L</p>
+                            <div class="card__hidden">
+                                <button class="card__button">Заказать</button>
+                            </div>
+                        </div> -->
+                    </div>
+                </section>
+            </div>
+        </div>
+        <footer class="page__footer footer">
+            <div class="footer__content">
+                <div class="footer__social social">
+                    <p class="social__copy">&copy; Kolesa Group</p>
+                    <div class="social__networks networks">
+                        <a class="networks__link" href="#"><img class="networks__img"
+                        src="./images/mainpage/instagram.svg"
+                        width="18px" height="18px" alt="instagram logo"></a>
+                        <a class="networks__link" href="#"><img class="networks__img"
+                        src="./images/mainpage/youtube.svg"
+                        width="18px" height="13px" alt="youtube logo"></a>
+                        <a class="networks__link" href="#"><img class="networks__img"
+                        src="./images/mainpage/vk.svg"
+                        width="18px" height="11px" alt="vk logo"></a>
+                    </div>
+                </div>
+                <div class="footer__feedback feedback">
+                    <div class="feedback__description">
+                        <p class="feedback__text">Есть идеи что улучшить?</p>
+                        <p class="feedback__text">Не знаешь с кем решить проблему?</p>
+                    </div>
+                    <button class="feedback__button">Написать</button>
+                </div>
+            </div>
+        </footer>
+    </div>
+    <!-- MODAL -->
+    <div class="pop pop__content js__pop">
+        <div class="modal js__modal">
+            <div class="modal__images">
+                <div class="main-image">
+                    <img class='js__modal-img' src="./images/modal/image1.svg" alt="cool shirt"
+                    width="330px" height="330px">
+                </div>
+                <div class="options">
+                    <div class="option1">
+                        <img src="./images/modal/option1.svg" alt="cool shirt 1"
+                        width="50px" height="50px">
+                    </div>
+                    <div class="option2 active">
+                        <img src="./images/modal/option2.svg" alt="cool shirt 2"
+                        width="50px" height="50px">
+                    </div>
+                    <div class="option3">
+                        <img src="./images/modal/option3.svg" alt="cool shirt 3"
+                        width="50px" height="50px">
+                    </div>
+                </div>
+            </div>
+            <div class="modal__description">
+                <h3 class="title">
+                    Футболка "Эволюционируй или сдохни"
+                </h3>
+                <div class="showcase">
+                    <div class="showcase-left">
+                        <p class="price">
+                            100 баллов
+                        </p>
+                        <button class="button" type="button">
+                            Заказать
+                        </button>
+                    </div>
+                    <div class="balance">
+                        <div class="current-balance">
+                            <p class="your-balance">Твой баланс:</p>
+                            <p>3 945 баллов</p>
+                        </div>
+                        <div class="bags">
+                            <img src="./images/modal/bags.png" alt="">
+                        </div>
+                    </div>
+                </div>
+                <p class="colors-title">
+                    Цвета:
+                </p>
+                <div class="colors">
+                    <input type="radio" name="color" id="blue">
+                    <label class="options blue" for="blue">Синий</label>
+                    <input type="radio" name="color" id="beige">
+                    <label class="options beige" for="beige">Бежевый</label>
+                    <input type="radio" name="color" id="gray">
+                    <label class="options gray" for="gray">Серый</label>
+                </div>
+                <p class="sizes-title">
+                    Размер:
+                </p>
+                <div class="sizes">
+                    <input type="radio" name="size" id="s">
+                    <label class="s" for="s">S</label>
+                    <input type="radio" name="size" id="m">
+                    <label class ="m" for="m">M</label>
+                    <input type="radio" name="size" id="l">
+                    <label class="l" for="l">L</label>
+                </div>
+                <div class="details">
+                    <h4 class="details-title">Детали:</h4>
+                    <p class="modal__dsc">Брендированная толстовка от Qazaq Republic.
+                    Материал: Хлопок 80%, Вискоза 20%</p>
+                </div>
+                <div class="choose-size">
+                    <h4 class="choose-title">Как выбрать размер:</h4>
+                    <p>Написать дяде Рику для уточнения.</p>
+                </div>
+            </div>
+            <div class="modal__close js__close-modal">
+                <img src="./images/modal/close.png" alt="" width="24px" height="24px">
+            </div>
+        </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld,
-  },
-};
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import './scss/style.scss';
 </style>
